82d6be2898619158e358ad55ab3628ae9a9d5280
Hodnjir <djornisson@gmail.com>
2024-05-03 12:27:46 +0200

    modified Pokemon class

--- code/polak.py
+++ code/polak.py
@@ -9,6 +9,23 @@ Created on Fri May  3 10:51:08 2024
 import csv
 import numpy as np
 
+
+
+
+class Pokemon:
+    def __init__(self,name,tp,pv,at,df,at_spc,df_spc,sp,coord = None):
+        self.name = name
+        self.tp = tp            #type
+        self.pv = pv            #points de vie
+        self.at = at            #attaque
+        self.df = df            #défense
+        self.at_spc = at_spc    #attaque spéciale
+        self.df_spc = df_spc    #défense spéciale
+        self.sp = sp            #vitesse(speed)
+        self.coord = coord
+
+
+
 Pokelist = []
 
 with open('../data/pokemon_first_gen.csv') as csvfile:
@@ -20,20 +37,16 @@ with open('../data/pokemon_first_gen.csv') as csvfile:
 Pokelist_legende = Pokelist.pop(0)
 
 
+        
+Pokedex = []
+
+for elt in Pokelist:
+    Pokedex.append(Pokemon(elt[0],elt[1],elt[2],elt[3],elt[4],elt[5],elt[6],elt[7]))
+    
+    
+    
+    
+    
 print(Pokelist)
 print(Pokelist_legende)
-
-
-
-class Pokemon:
-    def __init__(self,name,tp,pv,at,df,at_spc,df_spc,sp):
-        self.name = name
-        self.tp = tp            #type
-        self.pv = pv            #points de vie
-        self.at = at            #attaque
-        self.df = df            #défense
-        self.at_spc = at_spc    #attaque spéciale
-        self.df_spc = df_spc    #défense spéciale
-        self.sp = sp            #vitesse(speed)
-        
-        
+print(Pokedex[0].name)
