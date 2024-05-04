ed8802db53b3ea1eb7b1c3477f177fba9025de21
Baumgarten <romain.baumgarten@ensg.eu>
2024-05-04 14:30:27 +0200

    maj carte.py et deplacement.py
    le système de déplacement est fonctionnelle

--- carte/carte.py
+++ carte/carte.py
@@ -9,10 +9,7 @@ Created on Fri May  3 14:52:49 2024
 import numpy as np
 from PIL import Image
 
-im = np.array(Image.open("..\code\gui\Safari_Zone_entrance_RBY.png").convert('L'))
-im1 = np.array(Image.open("..\code\gui\Safari_Zone_area_1_RBY.png").convert('L'))
-im2 = np.array(Image.open("..\code\gui\Safari_Zone_area_2_RBY.png").convert('L'))
-im3 = np.array(Image.open("..\code\gui\Safari_Zone_area_3_RBY.png").convert('L'))
+
 
 def convertion_case(carte):
     n,m = np.shape(carte)
@@ -28,10 +25,7 @@ def convertion_case(carte):
             new_map[x,y] = int(moy/64)
     return new_map
 
-test = convertion_case(im)
-test1 = convertion_case(im1)
-test2 = convertion_case(im2)
-test3 = convertion_case(im3)
+
 
 class Area:
     def __init__(self,id,tab):
@@ -53,16 +47,26 @@ class Case:
         num_pix = self.area.tab[self.x,self.y]
         if num_pix == 183 or num_pix == 193:
             return "Herbe"
-        elif num_pix > 200:
+        elif num_pix > 210:
             return "sol"
         elif num_pix <= 175 and num_pix != 101:
             return "obstacle"
         else:
-            return "inconnu"
+            return "obstacle"
     
 
 
 if __name__ == "__main__":
+    im = np.array(Image.open("..\code\gui\Safari_Zone_entrance_RBY.png").convert('L'))
+    im1 = np.array(Image.open("..\code\gui\Safari_Zone_area_1_RBY.png").convert('L'))
+    im2 = np.array(Image.open("..\code\gui\Safari_Zone_area_2_RBY.png").convert('L'))
+    im3 = np.array(Image.open("..\code\gui\Safari_Zone_area_3_RBY.png").convert('L'))
+    
+    test = convertion_case(im)
+    test1 = convertion_case(im1)
+    test2 = convertion_case(im2)
+    test3 = convertion_case(im3)
+    
     test_area0 = Area(0,test)
     test_area1 = Area(1,test2)
     test_case = Case(10,6,test_area0)
--- carte/deplacement.py
+++ carte/deplacement.py
@@ -1,5 +1,6 @@
 
 import numpy as np
+from PIL import Image
 import carte as c
 
 class joueur:
@@ -30,10 +31,8 @@ class joueur:
                     self.case.x = 71
                     self.case.y = 42
             else:
-                case_bis = c.Case()
-                case_bis.x = x-1
-                case_bis.y = y
-                if self.case_bis.c.type_case(map) != "obstacle":
+                case_bis = c.Case(x-1,y,self.case.area)
+                if case_bis.type_case(self.map) != "obstacle":
                     self.case = case_bis
         if direction == "down":
             if x == 71:
@@ -50,12 +49,71 @@ class joueur:
                     self.case.x = 0
                     self.case.y = 29
             else:
-                case_bis = c.Case()
-                case_bis.x = x + 1
-                case_bis.y = y
-                if self.case_bis.c.type_case(map) != "obstacle":
+                case_bis = c.Case(x+1,y,self.case.area)
+                if case_bis.type_case(self.map) != "obstacle":
                     self.case = case_bis
         if direction == "left":
+            if y == 0:
+                if x >= 20 and x<= 23:
+                    self.case.area.id = 3
+                    self.case.x = 45
+                    self.case.y = 59
+                elif x >= 8 and x <= 11:
+                    self.case.area.id = 2
+                    self.case.x = 61
+                    self.case.y = 79
+                else:
+                    self.case.area.id = 0
+                    self.case.x = 21
+                    self.case.y = 59
+            else:
+                case_bis = c.Case(x,y-1,self.case.area)
+                if case_bis.type_case(self.map) != "obstacle":
+                    self.case = case_bis
+        if direction == "right":
+            if y == 59:
+                if self.case.area.id == 0:
+                    self.case.area.id = 1
+                    self.case.x = 45
+                    self.case.y = 0
+                elif self.case.area.id == 3:
+                    self.case.area.id = 0
+                    self.case.x = 21
+                    self.case.y = 0
+            elif y == 79:
+                self.case.area.id = 1
+                self.case.x = 9
+                self.case.y = 0
+            else:
+                case_bis = c.Case(x,y+1,self.case.area)
+                if case_bis.type_case(self.map) != "obstacle":
+                    self.case = case_bis
+                
+                    
+if __name__ == "__main__":
+    im = np.array(Image.open("..\code\gui\Safari_Zone_entrance_RBY.png").convert('L'))
+    im1 = np.array(Image.open("..\code\gui\Safari_Zone_area_1_RBY.png").convert('L'))
+    im2 = np.array(Image.open("..\code\gui\Safari_Zone_area_2_RBY.png").convert('L'))
+    im3 = np.array(Image.open("..\code\gui\Safari_Zone_area_3_RBY.png").convert('L'))
+    
+    test = c.convertion_case(im)
+    test1 = c.convertion_case(im1)
+    test2 = c.convertion_case(im2)
+    test3 = c.convertion_case(im3)
+    
+    test_area0 = c.Area(0,test)
+    test_area1 = c.Area(1,test1)
+    test_area2 = c.Area(2,test2)
+    test_area3 = c.Area(3,test3)
+    test_case = c.Case(10,6,test_area0)
+    test_map = c.Map([test_area0,test_area1, test_area2, test_area3])
+    
+    test_joueur = joueur(test_case, test_map)
+    
+    test_joueur.deplacement("up")
+    
+
             
+                    
             
                     
