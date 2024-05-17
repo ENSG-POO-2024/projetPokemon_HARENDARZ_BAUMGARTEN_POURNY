61564b1a2495bdcf2068d5aa38d0550ae760b270
Bastien Choulans <brucepourny@outlook.fr>
2024-05-17 19:47:45 +0200

    Update saving_mechanics.py

--- code/gui/saving_mechanics.py
+++ code/gui/saving_mechanics.py
@@ -2,7 +2,18 @@
 
 import csv
 
-global_variables = {}
+global_variables = {'Equipe': None, 'collection': None, 'environnement':None, 'nb_inventory':None, 'nb_team':None, 'slide':None, 'pix':None, 'piy':None, 'j1.map.area_tab':None, 'case_depart':None} 
+
+def fill_global_variables(global_variables):
+    for key in global_variables:
+        if key == 'area':
+            pass
+        if key == 'case_depart':
+            pass
+        else:
+            global_variables[key] = globals()[key]
+
+
 
 def save_game():
     pass
