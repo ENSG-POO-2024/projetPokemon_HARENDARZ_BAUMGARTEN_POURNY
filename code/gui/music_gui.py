13f38ae49a250fb15a28e5f76707e326dc7b10aa
Bruce Pourny <brucepourny@outlook.fr>
2024-05-17 17:22:23 +0200

    New musics

--- code/gui/music_gui.py
+++ code/gui/music_gui.py
@@ -16,11 +16,11 @@ class MusicJukebox(QMediaPlayer):
         super().__init__()
         self.currentVolume = 50
         self.setVolume(self.currentVolume)
-        self.songs_available = {'MainMenu_egg': './music/gta_3_theme.mp3'}
+        self.songs_available = {'MainMenu': './music/gta_3_theme.mp3', 'Exploration': './music/poke_diamond_pearl_great_marsh.mp3', 'Combat1': 'poke_black_white_battle', 'Combat2': 'cat_mario.mp3'}
         self.play_song()
 
 
-    def play_song(self, name='MainMenu_egg'):
+    def play_song(self, name='MainMenu'):
         song_path = QDir.current().absoluteFilePath(self.songs_available[name])
         print(song_path)
         song = QMediaContent(QUrl.fromLocalFile(song_path))
---
+++ code/gui/saving_mechanics.py
@@ -0,0 +1,8 @@
+# Saving mechanics
+
+import csv
+
+global_variables = {}
+
+def save_game():
+    pass
